08915b1894f3c10fdddec004b4a94d61877ba48a
Dako Albeik <dako.albeik@gmail.com>
2023-04-08 18:19:05 -0500

    Add deck class

---
+++ src/deck/deck.py
@@ -0,0 +1,28 @@
+import random
+
+from card import Rank, Suit, Card
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.set_cards()
+        self.shuffle()
+
+    def set_cards(self):
+        for suit in Suit:
+            for rank in Rank:
+                card = Card(rank, suit)
+                self.cards.append(card)
+
+    def __repr__(self):
+        string = ''
+        for card in self.cards:
+            string += card.__repr__() + " "
+
+        return string
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+
