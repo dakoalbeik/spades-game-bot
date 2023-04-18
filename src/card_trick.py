ab0477b3b526c9606b80290ad72a6bde8e9dc303
Dako Albeik <dako.albeik@gmail.com>
2023-04-17 21:30:04 -0500

    Fix determine winner function

--- src/card_trick.py
+++ src/card_trick.py
@@ -13,29 +13,25 @@ class CardTrick:
         return self.cards[0].suit if self.cards else None
 
     def determine_winner(self, lead_player):
-        # Initialize variables to store the highest card and player index
         highest_card = self.cards[0]
-        highest_player = 0
+        winning_player = 0
+        lead_suit = self.leading_suit
 
         # Iterate over the cards and find the highest card
-        for i in range(len(self.cards)):
-            card = self.cards[i]
-            suit = card.suit
-
-            # Check if the card is a trump card
-            if suit == Suit.SPADES and card.rank.value > highest_card.rank.value:
+        for i, card in enumerate(self.cards):
+            is_greater_rank = card.rank.value > highest_card.rank.value
+            no_spade = highest_card.suit != Suit.SPADES
+            # If card is greater spade
+            if card.suit == Suit.SPADES and (no_spade or is_greater_rank):
                 highest_card = card
-                highest_player = i
+                winning_player = i
 
-            # Check if the card is the highest of the leading suit
-            elif suit == self.leading_suit and card.rank.value > highest_card.rank.value:
+            # If card is greater lead suit
+            elif no_spade and card.suit == lead_suit and is_greater_rank:
                 highest_card = card
-                highest_player = i
+                winning_player = i
 
-        # Account for the lead player index
-        winning_player = (highest_player + lead_player) % 4
-
-        return winning_player
+        return (winning_player + lead_player) % 4
 
     def reset(self):
         self.cards.clear()
