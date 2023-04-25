d6f5aca2bb10e0dfab20a472c76636a870260a97
Dako Albeik <dako.albeik@gmail.com>
2023-04-25 18:53:10 -0500

    Add remaining code for fitting the model

--- dqn.py
+++ dqn.py
@@ -4,7 +4,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from rl.agents.dqn import DQNAgent
 from rl.memory import SequentialMemory
-from rl.policy import EpsGreedyQPolicy
+from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
+from tensorflow.keras.optimizers import Adam
 
 env = gym.make('spades:spades-v0')
 nb_actions = env.action_space.n
@@ -22,10 +23,15 @@ model.add(Dense(16, activation='relu'))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(nb_actions, activation='linear'))
 print(model.summary())
-# memory = SequentialMemory(limit=10000, window_length=1)
-# policy = EpsGreedyQPolicy(eps=0.1)
-# dqn = DQNAgent(model=model, nb_actions=env.action_space.n, memory=memory, nb_steps_warmup=10,
-#                target_model_update=1e-2, policy=policy)
-# dqn.compile(optimizer='adam', metrics=['mae'])
+memory = SequentialMemory(limit=100_000, window_length=1)
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(),
+                              attr='eps',
+                              value_max=1.,
+                              value_min=.1,
+                              value_test=.05,
+                              nb_steps=20000)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+               target_model_update=100, policy=policy)
+dqn.compile(optimizer=Adam(learning_rate=1e-3), metrics=['mae'])
 # # Train the agent
-# dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
+dqn.fit(flattened_env, nb_steps=10000, visualize=False, verbose=2)
